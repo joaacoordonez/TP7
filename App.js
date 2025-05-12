474830ac19528e6f93372144c8178044b1bc394f
Android Studio <devandroid@ort>
2025-05-12 12:10:56 -0300

    Avances

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView, Image } from 'react-native';
+
+const imgExterna = {uri: 'https://estaticos.elcolombiano.com/binrepository/848x565/68c0/780d565/none/11101/UPIA/foto-getty_45248333_20240531183800.jpg'}
 
 export default function App() {
   return (
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={styles.safeAreaView}>
+        <ImageBackground source={require('./assets/fondo.jpg')} style={styles.background} resizeMode="cover">
+          <StatusBar style="auto" />
+          <View style={styles.card}>
+            <Image source={imgExterna} style={styles.perfil}  />
+            <Text style={styles.nombre}>Juan Fernando Quintero</Text>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
     </View>
+    
   );
+  
 }
 
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    display: 'grid',
+    placeItems: 'center',
+    
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  card: {
+    marginTop: 100,
+    backgroundColor: '#fff',
+    width: 300,
+    height: 500,
+    display: 'grid',
+    placeItems: 'center',
+    borderRadius: '5%',
+  },
+  perfil: {
+    margin: 10,
+    width: 100,
+    height: 100,
+    borderRadius: '20%',
+  },
+  
+  nombre: {
+    fontFamily: 'Montserrat', 
+    fontSize: 24, 
+    fontWeight: '700', 
+    color: '#333', 
+    textAlign: 'center',  
+  }
 });
